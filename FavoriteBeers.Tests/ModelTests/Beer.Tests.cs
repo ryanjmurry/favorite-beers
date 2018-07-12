71de1265bf06fdbc7841126804d7d33ed2275f88
Ryan Murry <ryanjmurry@gmail.com>
2018-07-12 01:41:54 -0700

    add methods and tests for saving, deleting, and finding beers on server

--- FavoriteBeers.Tests/ModelTests/Beer.Tests.cs
+++ FavoriteBeers.Tests/ModelTests/Beer.Tests.cs
@@ -6,8 +6,64 @@ using FavoriteBeers.Models;
 namespace FavoriteBeers.Tests
 {
     [TestClass]
-    public class BeerTest
+    public class BeerTests : IDisposable
     {
+        //teardown method
+        public void Dispose()
+        {
+            Beer.DeleteAll();
+        }
 
+        //sets up connection to test database
+        public BeerTests()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=favorite_beer_test;";
+        }
+
+        [TestMethod]
+        public void GetAll_DbStartsEmpty_0()
+        {
+            int result = Beer.GetAll().Count;
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Equals_ReturnTrueIfBeersAreSame_Beers()
+        {
+            Beer firstBeer = new Beer(1, "Tecate", "Lager", 4.5, 15);
+            Beer secondBeer = new Beer(1, "Tecate", "Lager", 4.5, 15);
+            Assert.AreEqual(firstBeer, secondBeer);
+        }
+
+        [TestMethod]
+        public void Save_SavesToDatabase_BeerList()
+        {
+            Beer firstBeer = new Beer(1, "Tecate", "Lager", 4.5, 15);
+            firstBeer.Save();
+            List<Beer> testList = new List<Beer> { firstBeer }; 
+            List<Beer> result = Beer.GetAll();
+
+            CollectionAssert.AreEqual(testList, result);
+        }
+
+        [TestMethod]
+        public void Save_AssignsIdToObject_Id()
+        {
+            Beer testBeer = new Beer(1, "Tecate", "Lager", 4.5, 15);
+            testBeer.Save();
+            Beer savedBeer = Beer.GetAll()[0];
+            int result = savedBeer.Id;
+            int testId = testBeer.Id;
+            Assert.AreEqual(testId, result);
+        }
+
+        [TestMethod]
+        public void Find_FindsItemInDatabase_Beer()
+        {
+            Beer testBeer = new Beer(1, "Tecate", "Lager", 4.5, 15);
+            testBeer.Save();
+            Beer foundBeer = Beer.FindBeer(testBeer.Id);
+            Assert.AreEqual(testBeer, foundBeer);
+        }
     }
 }
--- FavoriteBeers/Models/Beer.cs
+++ FavoriteBeers/Models/Beer.cs
@@ -24,6 +24,30 @@ namespace FavoriteBeers.Models
             Id = newId;
         }
 
+        public override bool Equals(System.Object otherBeer)
+        {
+            if (!(otherBeer is Beer))
+            {
+                return false;
+            }
+            else
+            {
+                Beer newBeer = (Beer) otherBeer; //cast otherBeer as Beer object 
+                bool breweryIdEquality = (this.BreweryId == newBeer.BreweryId);
+                bool nameEquality = (this.Name == newBeer.Name);
+                bool styleEquality = (this.Style == newBeer.Style);
+                bool abvEquality = (this.ABV == newBeer.ABV);
+                bool ibuEquality = (this.IBU == newBeer.IBU);
+                bool IdEquality = (this.Id == newBeer.Id);
+                return (breweryIdEquality && nameEquality && styleEquality && abvEquality && ibuEquality);
+            }
+        }
+
+        // public override int GetHashCode()
+        // {
+        //     return this.Name.GetHashCode();
+        // }
+
         public static List<Beer> GetAll()
         {
             //setting up empty list of all beers
@@ -49,7 +73,7 @@ namespace FavoriteBeers.Models
                 int beerId = rdr.GetInt32(5);
 
                 //instantiates new Beer instance
-                Beer newBeer = new Beer(breweryId, beerName, beerStyle, beerABV, beerIBU, beerId)
+                Beer newBeer = new Beer(breweryId, beerName, beerStyle, beerABV, beerIBU, beerId);
 
                 //adds instance to list that contains every beer matching query
                 allBeers.Add(newBeer);
@@ -64,6 +88,83 @@ namespace FavoriteBeers.Models
             return allBeers;
         }
 
+        public static void DeleteAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
 
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM beers;";
+
+            //not searching, just deleting
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO beers (brewery_id, name, style, abv, ibu) VALUES (@breweryId, @beerName, @beerStyle, @beerABV, @beerIBU);";
+
+            //sets the instances property values as the values in the sql command
+            cmd.Parameters.AddWithValue("@breweryId", this.BreweryId);
+            cmd.Parameters.AddWithValue("@beerName", this.Name);
+            cmd.Parameters.AddWithValue("@beerStyle", this.Style);
+            cmd.Parameters.AddWithValue("@beerABV", this.ABV);
+            cmd.Parameters.AddWithValue("@beerIBU", this.IBU);
+
+            cmd.ExecuteNonQuery();
+            Id = (int) cmd.LastInsertedId; //cast LastInsertedId (returns long) to int and auto-increments record's id
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public static Beer FindBeer(int id)
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM beers WHERE id = @thisId;";
+            cmd.Parameters.AddWithValue("@thisId", id);
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+
+            //unsure why this is here...ask tomorrow
+            int breweryId = 0;
+            string beerName = "";
+            string beerStyle = "";
+            double beerABV = 0.0;
+            int beerIBU = 0;
+            int beerId = 0;
+
+            while(rdr.Read())
+            {
+                breweryId = rdr.GetInt32(0);
+                beerName = rdr.GetString(1);
+                beerStyle = rdr.GetString(2);
+                beerABV = rdr.GetDouble(3);
+                beerIBU = rdr.GetInt32(4);
+                beerId = rdr.GetInt32(5);
+            }
+
+            Beer foundBeer = new Beer(breweryId, beerName, beerStyle, beerABV, beerIBU, beerId);
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return foundBeer;
+        }
     }
 }
